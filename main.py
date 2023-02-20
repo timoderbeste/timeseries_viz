c4cd44203373a1663c8134aafe6796d3bef4d555
Timo Wang <ntwang1994@gmail.com>
2023-02-19 19:29:05 -0600

    Adding visualize_outliers function

--- main.py
+++ main.py
@@ -9,15 +9,33 @@ def main():
     time_viz = TimeseriesVisualizer(data_path, plt_style="ggplot")
     time_viz.visualize_distribution(
         t_col="LOCAL_DATE", 
-        d_cols=["CALGARY", "TORONTO", "VANCOUVER"], 
         t_keys=["01-Jan", "02-Feb"],
+        d_cols=["CALGARY", "TORONTO", "VANCOUVER"], 
         t_key_rule="contains",
         d_col_pattern="MEAN_TEMPERATURE_%s",
-        t_label="Year",
-        d_label="Mean Temperature",
+        x_label="Year",
+        y_label="Mean Temperature",
         save_path=os.path.join(data_dir, "distribution_plot.png"),
         show_plot=True,
     )
+
+    time_viz.visualize_outliers(
+        t_col="LOCAL_DATE",
+        t_keys=["01-Jan", "01-Feb", "01-Mar", "01-Apr", "01-May", "01-Jun",
+                "01-Jul", "01-Aug", "01-Sep", "01-Oct", "01-Nov", "01-Dec"],
+        d_cols=[
+        "TORONTO", 
+        "VANCOUVER", 
+        "CALGARY",
+        "HALIFAX"
+        ],
+        t_key_rule="contains",
+        d_col_pattern="MEAN_TEMPERATURE_%s",
+        x_label="Month of the year",
+        y_label="Mean Temperatures",
+        save_path=os.path.join(data_dir, "outliers_plot.png"),
+    )
+
     
 
 
--- timeseries_visualizer.py
+++ timeseries_visualizer.py
@@ -17,21 +17,12 @@ class TimeseriesVisualizer():
         plt.style.use(plt_style)
         
     def visualize_distribution(self,
-                               t_col, d_cols, t_keys,
+                               t_col, t_keys, d_cols,
                                t_key_rule="exact", d_col_pattern=None,
-                               t_label="", d_label="", 
+                               x_label="", y_label="", 
                                title=None, save_path=None, show_plot=False):
-        num_rows, num_cols = len(t_keys), len(d_cols)
-        fig, axs = plt.subplots(num_rows, num_cols, 
-                                figsize=(5 * num_cols, 5 * num_rows), 
-                                sharex=True,
-                                sharey=True,
-                                squeeze=False)
-        fig.add_subplot(111, frameon=False)
-        plt.tick_params(labelcolor="none", 
-                        top=False, bottom=False, left=False, right=False)
-        plt.xlabel(t_label)
-        plt.ylabel(d_label)
+        num_rows, num_cols, fig, axs = \
+            self.__initialize_2d_subplots(t_keys, d_cols, x_label, y_label)
         for i in range(num_cols):
             d_col = d_cols[i] 
             for j in range(num_rows):
@@ -39,9 +30,9 @@ class TimeseriesVisualizer():
                 axs[j, i].set_title("%s %s" % (d_col, t_key))
                 axs[j, i].grid(False)
                 timeseries_vals = \
-                    self.__extract_timeseries_vals(t_col, d_col, 
-                                                   t_key, t_key_rule, 
-                                                   d_col_pattern)
+                    self._extract_timeseries_vals(t_col, t_key,
+                                                  d_col, t_key_rule, 
+                                                  d_col_pattern)
                 valid_idx = np.isfinite(timeseries_vals)
                 valid_timeseries_vals = timeseries_vals[valid_idx]
                 m, b = np.polyfit(range(len(valid_timeseries_vals)), 
@@ -62,8 +53,63 @@ class TimeseriesVisualizer():
         if save_path is not None:
             fig.savefig(save_path)
         fig.clf()
+
+
+    def visualize_outliers(self,
+                           t_col, t_keys, d_cols,
+                           t_key_rule="exact", d_col_pattern=None,
+                           x_label="", y_label="", 
+                           title=None, save_path=None, show_plot=False):
+        num_rows, fig, axs = \
+            self.__initialize_1d_subplots(d_cols, x_label, y_label)
+        
+        for i in range(num_rows):
+            d_col = d_cols[i]
+            axs[i, 0].set_title("%s" % (d_col))
+            axs[i, 0].grid(False)
+            t_key2timeseries_vals = dict()
+            for t_key in t_keys:
+                timeseries_vals = \
+                    self._extract_timeseries_vals(t_col, t_key,
+                                                  d_col, t_key_rule, 
+                                                  d_col_pattern)
+                t_key2timeseries_vals[t_key] = timeseries_vals
+            axs[i, 0].boxplot(x=t_key2timeseries_vals.values())
+        
+        if title is None:
+            title = "OUTLIERS" \
+                if not d_col_pattern \
+                else "%s OUTLIERS" % d_col_pattern.replace("%s", "").strip("-_ ")
+        fig.suptitle(title)
+
+        if show_plot:
+            fig.show()
+        if save_path is not None:
+            fig.savefig(save_path)
+        fig.clf()
+
+
+    # def visualize_correlations(self,
+    #                            t_col, t_keys, 
+    #                            d_cols1, d_cols2, 
+    #                            t_key_rule="exact", 
+    #                            d_col_pattern1=None, d_col_pattern2=None,
+    #                            x_label="", y_label="",
+    #                            title=None, save_path=None, show_plot=False):
+    #     def compute_correlations_for_t_key(t_key):
+    #         d_col1_data = []
+    #         d_col2_data = []
+    #         for d_col1 in d_cols1:
+    #             d_col1_data.extend(self._extract_timeseries_vals(
+    #                 t_col=t_col,
+    #                 t_key=t_key,
+    #                 d_col=d_col1,
+
+    #             ))
+    #     pass
     
-    def __extract_timeseries_vals(self, t_col, d_col, t_key, t_key_rule, d_col_pattern):
+    def _extract_timeseries_vals(self, t_col, t_key, d_col, 
+                                 t_key_rule, d_col_pattern, drop_na=True):
         if t_key is not None:
             if t_key_rule == "exact":
                 df = self.df.loc[self.df[t_col] == t_key]
@@ -75,7 +121,43 @@ class TimeseriesVisualizer():
             df = self.df
             
         if d_col_pattern is not None:
-            return np.array(df[d_col_pattern % d_col])
+            try:
+                formatted_d_col = d_col_pattern % d_col
+            except TypeError as e:
+                print(e)
+                print("d_col", d_col)
+                print("d_col_pattern", d_col_pattern)
+                exit(1)
+            return np.array(df[formatted_d_col].dropna()) \
+                if drop_na else np.array(df[formatted_d_col].dropna())
         else:
-            return np.array(df[d_col])
+            return np.array(df[d_col].dropna()) \
+                if drop_na else np.array(df[d_col])
     
+    def __initialize_2d_subplots(self, t_keys, d_cols, x_label, y_label):
+        num_rows, num_cols = len(t_keys), len(d_cols)
+        fig, axs = plt.subplots(num_rows, num_cols, 
+                                figsize=(5 * num_cols, 5 * num_rows), 
+                                sharex=True,
+                                sharey=True,
+                                squeeze=False)
+        fig.add_subplot(111, frameon=False)
+        plt.tick_params(labelcolor="none", 
+                        top=False, bottom=False, left=False, right=False)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        return num_rows,num_cols,fig,axs
+    
+    def __initialize_1d_subplots(self, items, x_label, y_label):
+        num_rows = len(items)
+        fig, axs = plt.subplots(num_rows, 
+                                figsize=(10, 5 * num_rows), 
+                                sharex=True,
+                                sharey=True,
+                                squeeze=False)
+        fig.add_subplot(111, frameon=False)
+        plt.tick_params(labelcolor="none", 
+                        top=False, bottom=False, left=False, right=False)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
+        return num_rows, fig, axs
